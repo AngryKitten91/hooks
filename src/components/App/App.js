a0b20ae3ffd0f7733b8a057421fde15dbbdc46a3
Bartłomiej Klęba <bartkof91@gmail.com>
2019-03-02 12:54:54 +0100

    more hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,8 +23,8 @@ export default class App extends Component {
     return (
       <div className="App">
         <h3>Class state</h3>
-        <div>You clicked {count} times</div>
-        <div onClick={this.handleAdd}>Add</div>
+        <p>You clicked {count} times</p>
+        <p onClick={this.handleAdd}>Add</p>
         <h3>Function state</h3>
         <Example />
       </div>
@@ -35,14 +35,33 @@ export default class App extends Component {
 
 function Example() {
   
-  const [count, setCount] = useState(0);
+  const [counter, setCount] = useState(0);
+
+  const [todos, setTodos] = useState(['item1']);
+
+  const add = () => setCount(counter + 1)
+  const subtract = () => setCount(counter - 1)
+
+  const addTodo = (item) =>{
+    setTodos([...todos, item])
+  }
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <p onClick={() => setCount(count + 1)}>
+      <p>You clicked {counter} times</p>
+      <p onClick={add}>
         Add
       </p>
+      <p onClick={subtract}>
+        Subtract
+      </p>
+      <h3>TODOS</h3>
+      <p onClick={()=>{
+        addTodo("item2")
+      }}>Add Item</p>
+      {todos.map(elem => {
+        return <div>{elem}</div>
+      })}
     </div>
   );
 }
